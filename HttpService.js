54a41f31ab542ac4fbf6a8349427a5598950e4f6
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-09-25 15:18:39 +0800

    add method to get click & module http end

--- HttpService.js
+++ HttpService.js
@@ -26,6 +26,7 @@ class HttpService {
         this.app.put('/session', HttpService.auth_session);
         this.app.get('/session', HttpService.check_session);
         this.app.get('/transfer_id', HttpService.generate_transfer_id);
+        this.app.get('/click', HttpService.get_click);
 
         this.app.listen(8080);
         console.log('[HttpService] Listening on port 8080');
@@ -33,6 +34,17 @@ class HttpService {
 
     static async new() {
         return new HttpService();
+    }
+
+    /**
+     * @param {Express.Response} res
+     * @param {number} status
+     * @param {Object} json
+     */
+    static endAndSend(res, status, json) {
+        res.status(status);
+        res.json(json);
+        res.end();
     }
 
     /**
@@ -44,13 +56,9 @@ class HttpService {
         var sessionService = ServiceReferences.instance.SessionService;
         var isSessionInit = await sessionService.isSesionInit(req.session.id);
         if (!isSessionInit) {
-            res.status(401);
-            res.json({error: 'Not logged in.'});
-            return res.end();
+            return HttpService.endAndSend(res, 401, {error: 'Not logged in.'});
         }
-        res.status(200);
-        res.json({});
-        return res.end();
+        return HttpService.endAndSend(res, 200, {});
     }
 
     /**
@@ -63,11 +71,10 @@ class HttpService {
         var sessionService = ServiceReferences.instance.SessionService;
         var user = await dbService.newUser();
         await sessionService.initSession(req.session.id, user.uid, user.clickCount);
-        res.json({
+        return HttpService.endAndSend(res, 200, {
             uid: user.uid,
             login_token: user.loginToken
-        })
-        res.status(200);
+        });
     }
 
     /**
@@ -86,30 +93,23 @@ class HttpService {
             if (user.loginToken != loginToken) throw new Error('');
         }
         catch (e) {
-            res.status(401);
-            res.json({error: 'Invalid login token.'});
-            return res.end();
+            return HttpService.endAndSend(res, 401, {error: 'Invalid login token.'});
         }
         await sessionService.initSession(req.session.id, user.uid, user.clickCount);
-        res.status(200);
-        res.json({})
-        res.end();
+        return HttpService.endAndSend(res, 200, {});
     }
 
     /**
      * @private
      * @param {Express.Request} req 
      * @param {Express.Response} res 
-     * @returns 
      */
     static async generate_transfer_id(req, res) {
         var dbService = ServiceReferences.instance.DatabaseService;
         var sessionService = ServiceReferences.instance.SessionService;
         var isSesionInit = await sessionService.isSesionInit(req.session.id);
         if (!isSesionInit) {
-            res.status(401);
-            res.json({error: 'Not logged in.'});
-            return res.end();
+            return HttpService.endAndSend(res, 401, {error: 'Not logged in.'});
         }
         var uid = await sessionService.getSessionUid(req.session.id);
 
@@ -118,9 +118,7 @@ class HttpService {
             transferId = await dbService.newTransferId(uid);
         }
         catch (e) {
-            res.status(500);
-            res.json({error: "Invalid user."});
-            return res.end();
+            return HttpService.endAndSend(res, 500, {error: "Invalid user."});
         }
         
         res.json({transfer_id: transferId});
@@ -138,23 +136,17 @@ class HttpService {
 
         var transferId = req.body.transfer_id;
         if (!transferId) {
-            res.status(400);
-            res.json({error: 'No transfer ID provided.'});
-            return res.end();
+            return HttpService.endAndSend(res, 400, {error: 'No transfer ID provided.'});
         }
 
         var uidToChange = req.body.uid;
         if (!uidToChange) {
-            res.status(400);
-            res.json({error: 'No UID provided.'});
-            return res.end();
+            return HttpService.endAndSend(res, 400, {error: 'No UID provided.'});
         }
 
         var uidCurrent = await sessionService.getSessionUid(req.session.id);
         if (uidCurrent && uidCurrent == uidToChange) {
-            res.status(400);
-            res.json({error: 'Cannot transfer to self.'});
-            return res.end();
+            return HttpService.endAndSend(res, 400, {error: 'Cannot transfer to self.'});
         }
 
         var user;
@@ -163,9 +155,7 @@ class HttpService {
             if (user.transferId != transferId) throw new Error('');
         }
         catch (e) {
-            res.status(400);
-            res.json({error: 'Invalid user or transfer ID.'});
-            return res.end();
+            return HttpService.endAndSend(res, 400, {error: 'Invalid user or transfer ID.'});
         }
         
         if (uidCurrent) await dbService.deleteUser(uidCurrent);
@@ -173,9 +163,7 @@ class HttpService {
             await dbService.resetTransferId(uidToChange);
         }
         catch (e) {
-            res.status(500);
-            res.json({error: 'Could not reset transfer ID.'});
-            return res.end();
+            return HttpService.endAndSend(res, 500, {error: 'Could not reset transfer ID.'});
         }
 
         var clickToInit;
@@ -189,11 +177,32 @@ class HttpService {
         await sessionService.transferSession(req.session.id, user.uid, clickToInit);
         var updatedToken = await dbService.updateToken(uidToChange);
 
-    
-        res.json({token: updatedToken});
-        res.status(200);
+        return HttpService.endAndSend(res, 200, {token: updatedToken});
     }
 
+    /**
+     * @private
+     * @param {Express.Request} req
+     * @param {Express.Response} res
+     */
+    static async get_click(req, res) {
+        var sessionService = ServiceReferences.instance.SessionService;
+        var dbService = ServiceReferences.instance.DatabaseService;
+        
+        var sessionId = req.session.id;
+        if (!await sessionService.isSesionInit(sessionId)) {
+            return HttpService.endAndSend(res, 401, {error: 'Not logged in.'});
+        }
+
+        var clickCount = await sessionService.getSessionClicks(sessionId);
+        return HttpService.endAndSend(res, 200, {clicks: clickCount});
+    }
+
+    /**
+     * @private
+     * @param {Express.Request} req
+     * @param {Express.Response} res
+     */
     static async add_click(req, res) {
         var sessionService = ServiceReferences.instance.SessionService;
         var dbService = ServiceReferences.instance.DatabaseService;
