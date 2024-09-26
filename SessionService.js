402a7a77e3b4554765feb5a0a6597c7d8d0f0275
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-09-26 21:59:37 +0800

    add password for redis

--- SessionService.js
+++ SessionService.js
@@ -1,5 +1,6 @@
 const Redis = require('redis');
 const Utils = require('./Utils');
+const dotenv = require('dotenv');
 
 class SessionService {
     /** @type {Redis.RedisClientType} */
@@ -10,62 +11,67 @@ class SessionService {
 
     static async new() {
         var result = new SessionService();
-        result.client = Redis.createClient();
+        var redisPwd = dotenv.config().parsed["redis-pwd"];
+        var redisConfig = {}
+        if (redisPwd) {
+            redisConfig.password = redisPwd;
+        }
+        result.client = Redis.createClient(redisConfig);
         result.client.connect();
         console.log('[SessionService] Connected to Redis');
         return result;
     }
 
     async isSesionInit(sessionId) {
-        return await this.client.EXISTS(`session:${sessionId}`);
+        return await this.client.EXISTS(`pn::session:${sessionId}`);
     }
 
     async isUserInit(uid) {
-        return await this.client.EXISTS(`uid:${uid}`);
+        return await this.client.EXISTS(`pn::uid:${uid}`);
     }
     
     async initSession(sessionId, uid, count) {
-        await this.client.SET(`session:${sessionId}`, uid);
-        await this.client.SET(`clicks:${sessionId}`, count);
-        await this.client.SET(`uid:${uid}`, sessionId);
+        await this.client.SET(`pn::session:${sessionId}`, uid);
+        await this.client.SET(`pn::clicks:${sessionId}`, count);
+        await this.client.SET(`pn::uid:${uid}`, sessionId);
     }
 
     async transferSession(sessionId, newUid, newCount) {
         try{
-            var oldSession = await this.client.GET(`uid:${newUid}`);
-            await this.client.DEL(`session:${oldSession}`);
-            await this.client.DEL(`clicks:${oldSession}`);
+            var oldSession = await this.client.GET(`pn::uid:${newUid}`);
+            await this.client.DEL(`pn::session:${oldSession}`);
+            await this.client.DEL(`pn::clicks:${oldSession}`);
         }
         catch(ignored){}
-        await this.client.SET(`session:${sessionId}`, newUid);
-        await this.client.SET(`clicks:${sessionId}`, newCount);
-        await this.client.SET(`uid:${newUid}`, sessionId);
+        await this.client.SET(`pn::session:${sessionId}`, newUid);
+        await this.client.SET(`pn::clicks:${sessionId}`, newCount);
+        await this.client.SET(`pn::uid:${newUid}`, sessionId);
     }
 
     async getUidSession(uid) {
         if (!await this.isUserInit(uid)) throw new Error('User not initialized.');
-        return await this.client.GET(`uid:${uid}`);
+        return await this.client.GET(`pn::uid:${uid}`);
     }
 
     async getUidClicks(uid) {
         if (!await this.isUserInit(uid)) throw new Error('User not initialized.');
         var sessionId = await this.getUidSession(uid);
-        return await this.client.GET(`clicks:${sessionId}`);
+        return await this.client.GET(`pn::clicks:${sessionId}`);
     }
 
     async getSessionUid(sessionId) {
         if (!await this.isSesionInit(sessionId)) throw new Error('Session not initialized.');
-        return await this.client.GET(`session:${sessionId}`);
+        return await this.client.GET(`pn::session:${sessionId}`);
     }
 
     async getSessionClicks(sessionId) {
         if (!await this.isSesionInit(sessionId)) throw new Error('Session not initialized.');
-        return await this.client.GET(`clicks:${sessionId}`);
+        return await this.client.GET(`pn::clicks:${sessionId}`);
     }
 
     async addClick(sessionId, count) {
         if (!await this.isSesionInit(sessionId)) throw new Error('Session not initialized.');
-        await this.client.INCRBY(`clicks:${sessionId}`, count);
+        await this.client.INCRBY(`pn::clicks:${sessionId}`, count);
     }
 
     // async newSession()
